68dac138fc7fb1dbaff369e7f2e4ab0b277162ec
smartdev1206 <tree1206@outlook.com>
2021-04-20 15:23:50 +0700

    fix BACKEND_URL

--- env-config.js
+++ env-config.js
@@ -3,8 +3,8 @@ console.log("process.env.NODE_ENV", process.env.NODE_ENV, prod)
 
 module.exports = {
     // BACKEND_URL: 'http://localhost:8000',
-    // BACKEND_URL: 'http://stars-website-react-2.ue.r.appspot.com',
-    BACKEND_URL: prod ? 'http://stars-website-react-2.ue.r.appspot.com' : 'http://localhost:8000',
+    // BACKEND_URL: 'https://stars-website-react-2.ue.r.appspot.com',
+    BACKEND_URL: prod ? 'https://stars-website-react-2.ue.r.appspot.com' : 'http://localhost:8000',
     RECAPTCHA_KEY: "6LffDNoZAAAAAA3a5FIcMwHJSB3V4wJ5_U5w60GH",
     PIXEL_ID: "339821513993100",
     GA_ID: "UA-129107246-1",
