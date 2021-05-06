166310f7adaba5a0c42ed08f7d07a99896851cdc
smartdev1206 <tree1206@outlook.com>
2021-05-07 00:08:29 +0700

    update api url

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    BACKEND_URL: prod ? 'http://localhost' : 'http://localhost:8000',
+    BACKEND_URL: prod ? 'http://ec2-3-17-142-202.us-east-2.compute.amazonaws.com' : 'http://localhost:8000',
     RECAPTCHA_KEY: "6LffDNoZAAAAAA3a5FIcMwHJSB3V4wJ5_U5w60GH",
     PIXEL_ID: "339821513993100",
     GA_ID: "UA-129107246-1",
