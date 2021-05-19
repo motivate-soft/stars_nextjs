d7fd415f3736d149d628d4271465cf7dbc71c8f8
smartdev1206 <tree1206@outlook.com>
2021-05-19 21:27:16 +0700

    update github workflow, deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-sudo su -
-cd /var/starsofboston/stars_nextjs
-git pull origin master
-npm install --legacy-peer-deps 
-npm run build
-pm2 restart next
+sudo git pull origin master
+sudo npm install --legacy-peer-deps 
+sudo npm run build
+sudo pm2 restart next
