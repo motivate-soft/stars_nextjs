d2c60cfe194596f55fba77ea57361393acf5f45e
smartdev1206 <tree1206@outlook.com>
2023-02-20 08:58:41 +0700

    - enable google analytics

--- components/HOC/WithGATracker.js
+++ components/HOC/WithGATracker.js
@@ -1,25 +1,28 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from 'react';
 import Router from "next/router";
 import ReactGA from "react-ga";
-import { GA_ID } from "../../env-config";
-import Analytics from "analytics";
-import googleTagManager from "@analytics/google-tag-manager";
+import {GA_ID} from "../../env-config";
 
-const analytics = Analytics({
-  app: "Stars of Boston",
-  plugins: [
-    googleTagManager({
-      containerId: GA_ID,
-    }),
-  ],
-});
+const logPageView = () => {
+    ReactGA.set({page: window.location.pathname})
+    ReactGA.pageview(window.location.pathname)
+}
 
-const WithGATracker = (WrappedComponent) => (props) => {
-  useEffect(() => {
-    analytics.page();
-  }, [Router.events]);
+export default (WrappedComponent) => (props) => {
+    useEffect(() => {
+        ReactGA.initialize(GA_ID)
+        if (!Router.asPath.includes('?')) {
+            logPageView()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-  return <WrappedComponent {...props} />;
+    useEffect(() => {
+        Router.events.on('routeChangeComplete', logPageView)
+        return () => {
+            Router.events.off('routeChangeComplete', logPageView)
+        }
+    }, [Router.events])
+
+    return <WrappedComponent {...props} />;
 };
-
-export default WithGATracker;
